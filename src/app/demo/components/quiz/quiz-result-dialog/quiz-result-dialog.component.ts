b52e01592ae36cb51baa754614ed3dde88d7f0b0
elyes2dev <elyes.zoghlami.1@esprit.tn>
2025-04-05 19:27:48 +0100

    gonna start designing but everything works

---
+++ src/app/demo/components/quiz/quiz-result-dialog/quiz-result-dialog.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { UserQuizScoreService } from 'src/app/demo/services/user-quiz-score.service';
+import { QuestionService } from 'src/app/demo/services/question.service';
+
+@Component({
+  selector: 'app-quiz-result-dialog',
+  templateUrl: './quiz-result-dialog.component.html',
+  styleUrls: ['./quiz-result-dialog.component.scss']
+})
+export class QuizResultDialogComponent implements OnInit {
+  quizId!: number;
+  userId!: number;
+  score!: number;
+  questions: any[] = [];
+  loading = true;
+
+  constructor(
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig,
+    private userQuizScoreService: UserQuizScoreService,
+    private questionService: QuestionService
+  ) {
+    this.quizId = this.config.data.quizId;
+    this.userId = this.config.data.userId;
+    this.score = this.config.data.score;
+  }
+
+  ngOnInit(): void {
+    this.loadQuizResults();
+  }
+
+  loadQuizResults(): void {
+    this.questionService.getQuestionsByQuizId(this.quizId).subscribe({
+      next: (questions) => {
+        this.questions = questions.map(q => ({
+          ...q,
+          userAnswer: null, // You'll need to implement this based on your data model
+          isCorrect: null   // You'll need to implement this based on your data model
+        }));
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+}
